c9f58f2725b691762abc1cc3539f8892ba13883f
Darren Maczka <dmaczka@vt.edu>
2018-07-19 13:47:20 -0400

    fix example config

--- scripts/build-jekyll.sh
+++ scripts/build-jekyll.sh
@@ -7,18 +7,6 @@ giturl=$3
 source=$4
 build=$5
 
-# Check to see if repo exists. If not, git clone it
-# and run nginx setup
-if [ ! -d $source ]; then
-    git clone $giturl $source
-fi
-
-# Git checkout appropriate branch, pull latest code
-cd $source
-git fetch --all
-git reset --hard origin/$branch
-cd -
-
 # Run jekyll
 cd $source
 $HOME/.rvm/gems/ruby-2.3.0/wrappers/jekyll build -s $source -d $build
